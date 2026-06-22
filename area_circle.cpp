67fb11b94c5ed036b48dc01e2fe15ba2610f9d58
ayush-yad-av <ay.yadav1317@gmail.com>
2026-06-22 23:25:15 +0530

    Add files via upload

---
+++ area_circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float radius ,area;
+    cout<<"enter radius of circle =";
+    cin>>radius;
+    area= 3.14*radius*radius;
+    cout<<"area of circle is="<<area<<endl;
+    return 0;
+}
+
+
+
